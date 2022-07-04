7b0ebc1d663a2a8004fed009d0e0c7033f4e1514
maso <mostafa.barmshory@gmail.com>
2022-07-04 15:18:24 +0430

    Init data model

---
+++ beton_stock/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Beton Stock improve",
+    "summary": "Summary",
+    "author": "ViraWeb123",
+    "website": "https://viraweb123.ir",
+    "license": "AGPL-3",
+    "category": "Delivery",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "stock",
+        "res",
+        "fleet"
+    ],
+    "data": [
+        "views/beton_stock_move_line_view.xml",
+    ],
+    'auto_install': False,
+    'installable': True,
+    'images': ['static/description/assets/img/main_screenshot.png']
+}
---
+++ beton_stock/models/__init__.py
@@ -0,0 +1,1 @@
+from . import beton_stock_move_line
