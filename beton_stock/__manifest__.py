c8b29c49c5af4fb779d894c10c6447e3aae138ea
maso <mostafa.barmshory@gmail.com>
2022-07-04 17:13:57 +0430

    add view to extend stock.move.line.operations.tree

--- beton_stock/__manifest__.py
+++ beton_stock/__manifest__.py
@@ -7,14 +7,12 @@
     "category": "Delivery",
     "version": "14.0.1.0.0",
     "depends": [
+        "base",
         "stock",
-        "res",
         "fleet"
     ],
     "data": [
-        "views/beton_stock_move_line_view.xml",
     ],
     'auto_install': False,
-    'installable': True,
-    'images': ['static/description/assets/img/main_screenshot.png']
+    'installable': True
 }
--- beton_stock/models/beton_stock_move_line.py
+++ beton_stock/models/beton_stock_move_line.py
@@ -5,8 +5,10 @@ from odoo.exceptions import UserError, ValidationError
 from odoo.tools import OrderedSet
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
+
 class BetonStockMoveLine(models.Model):
-    _name = "bton.stock.move.line"
+    _inherit = ["stock.move.line"]
+    _name = "beton.stock.move.line"
     _description = "Product Moves (Stock Move Line)"
     
     vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', readonly=False, required=True, index=True)
