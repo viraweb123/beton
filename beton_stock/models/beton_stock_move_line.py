d9f1e6901f0ce3b1f75ef25316a7b40c82727ac8
Masood <masoodzarei64@gmail.com>
2022-07-06 10:37:47 +0430

    fixed: inheritance does not need name or description

--- beton_stock/models/beton_stock_move_line.py
+++ beton_stock/models/beton_stock_move_line.py
@@ -8,8 +8,6 @@ from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 class BetonStockMoveLine(models.Model):
     _inherit = ["stock.move.line"]
-    _name = "beton.stock.move.line"
-    _description = "Product Moves (Stock Move Line)"
     
-    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', readonly=False, required=True, index=True)
-    driver_id = fields.Many2one('res.partner', string='Driver', readonly=False, required=True, index=True)
+    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', readonly=False, required=False, index=True)
+    driver_id = fields.Many2one('res.partner', string='Driver', readonly=False, required=False, index=True)
