d7533469c2950b282f071374ba65895d3cc71cc2
Masood <masoodzarei64@gmail.com>
2022-07-23 14:17:19 +0430

    print button added to move line view

--- beton_stock/models/beton_stock_move_line.py
+++ beton_stock/models/beton_stock_move_line.py
@@ -13,4 +13,15 @@ class BetonStockMoveLine(models.Model):
     driver_id = fields.Many2one('res.partner', string='Driver', readonly=False, required=False, index=True)
 
     empty_weight_id = fields.Float(string='Empty Weight', size=16, digits=(16, 0), required=False)
-    full_weight_id = fields.Float(string='Full weight', size=16, digits=(16, 0), required=False)
+    full_weight_id = fields.Float(string='Full Weight', size=16, digits=(16, 0), required=False)
+
+    def print_move_line_action(self, access_uid=None):
+        return {
+            'type': 'ir.actions.report',
+            'model': 'stock.move.line',
+            'name': 'stock.move.line',
+            'report_type': 'qweb-pdf',
+            'binding_model_id': 'model_stock.move.line',
+            'report_name': 'beton_stock.report_moveline_id_card',
+            'report_file': 'beton_stock.report_moveline_id_card'
+        }
